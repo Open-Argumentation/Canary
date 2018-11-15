8c930180ff144ebab94dbe5535683e9cc47f3fc0
Calvin-Castle Gill <calvincgill@gmail.com>
2018-11-15 22:51:01 +0000

    Basic routing, HTML and CSS added.

--- index.py
+++ index.py
@@ -1,14 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 from canary import Tokenizer
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def root():
+    return render_template('index.html')
+
+@app.route('/home')
+def home():
     return Tokenizer('Hello - Canary')
 
 @app.errorhandler(404)
 def error_404(error):
-    return "Error 404 - Canary", 404   
+    return render_template('404.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
