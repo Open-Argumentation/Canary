ef9e1d032a14281466bf908016f24ee1f612cfb7
Calvin-Castle Gill <calvincgill@gmail.com>
2019-01-09 17:58:04 +0000

    Major Updates since last Commit: Working implementation of a Argument Miner using Discourse Indicators, fully annotated corpus and minimal work done to website.

--- canary.py
+++
@@ -1,6 +0,0 @@
-from nltk.tokenize import word_tokenize
-
-def Tokenizer(string):
-    tokens = word_tokenize(string)
-    print("Tokenizing Input...")
-    return str(tokens)
--- index.py
+++ index.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
-from canary import Tokenizer
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,9 +15,20 @@ def home():
 def tutorial():
     return render_template('tutorial.html')
 
-@app.route('/tokenizer')
-def tokenizer():
-    return Tokenizer('Hello - Canary')
+@app.route('/input')
+def input():
+    return render_template('input.html')
+
+@app.route('/application')
+def application():
+    return render_template('application.html')
+
+@app.route('/upload', methods = ['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return ('file uploaded: ' + str(f.filename))
 
 @app.errorhandler(404)
 def error_404(error):
