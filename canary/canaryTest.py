df8af639153e1a16b91f6b606e3e288bcb5b0952
Calvin-Castle Gill <calvincgill@gmail.com>
2019-03-11 09:39:13 +0000

    Now displays all Argumentative Components when comparing. Changed arguments to allow for both .txt and .ann files

--- canary/canaryTest.py
+++ canary/canaryTest.py
@@ -58,11 +58,11 @@ def readAnn(file):
 
     return components
 
-def canaryBratAnalysis(file):
+def canaryBratAnalysis(fileTxt, fileAnn):
     """ Used to compare the outputs of Canary with a manually annotated Gold Standard """
 
     # Loading file into the local version of Canary
-    canary = canaryLocal(file)
+    canary = canaryLocal(fileTxt)
 
     # Used to store Argumentative Components
     majorClaims = canary[1]
@@ -70,7 +70,7 @@ def canaryBratAnalysis(file):
     premises = canary[2]
     
     # Reading analysis file and extracting components
-    analysis = readAnn("../corpus/essay001.ann")
+    analysis = readAnn(fileAnn)
 
     # Used to store Argumentative Components from analysis
     majorClaimsAnn = analysis[1]
@@ -78,28 +78,56 @@ def canaryBratAnalysis(file):
     premisesAnn = analysis[2]
 
     # Stores counts used to compare findings
+    majorCount = 0
+    majorCountAnn = 0
     claimCount = 0
     claimCountAnn = 0
+    premiseCount = 0
+    premiseCountAnn = 0
+    
 
     # Main loop to compare findings of Canary with Gold Standard    
+    for majorClaimAnn in majorClaimsAnn:
+        majorCountAnn += 1
+        for majorClaim in majorClaims:
+            if majorClaim.lower() in majorClaimAnn.lower() or majorClaimAnn.lower() in majorClaim.lower():
+                print("MATCH FOUND MAJOR CLAIM!")
+                print("Canary: " + majorClaim)
+                print("Analysis: " + majorClaimAnn)
+                print("\n")
+                # Found a match, increment Canary score
+                majorCount += 1
+                
     for claimAnn in claimsAnn:
         # Increment Count for analysis claim
         claimCountAnn += 1
         for claim in claims:
-            if claim in claimAnn or claimAnn in claim:
-                print("MATCH FOUND!")
+            if claim.lower() in claimAnn.lower() or claimAnn.lower() in claim.lower():
+                print("MATCH FOUND CLAIM!")
                 print("Canary: " + claim)
                 print("Analysis: " + claimAnn)
                 print("\n")
                 # Found a match, increment Canary score
                 claimCount += 1
 
+    for premiseAnn in premisesAnn:
+        premiseCountAnn += 1
+        for premise in premises:
+            if premise.lower() in premiseAnn.lower() or premiseAnn.lower() in premise.lower():
+                print("MATCH FOUND Premise!")
+                print("Canary: " + premise)
+                print("Analysis: " + premiseAnn)
+                print("\n")
+                # Found a match, increment Canary score
+                premiseCount += 1
+
+    print("Canary vs Brat (Major Claims): " + str(majorCount) + "/" + str(majorCountAnn))
     print("Canary vs Brat (Claims): " + str(claimCount) + "/" + str(claimCountAnn))
-
-
+    print("Canary vs Brat (Premise): " + str(premiseCount) + "/" + str(premiseCountAnn))
+    
 if __name__ == '__main__':
     """ Used to test the various features of Canary """
     
-    canaryBratAnalysis("../corpus/essay001.txt")
+    canaryBratAnalysis("../corpus/essay001.txt", "../corpus/essay001.ann")
     
 
