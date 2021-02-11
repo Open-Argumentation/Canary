3bfac33daeb6a8eeba91b5d4fe74a619a1269959
Christopher Wales <chriswales95@outlook.com>
2021-02-11 21:58:49 +0000

    Add the functionality to download limited corpora

---
+++ tests/test___download__.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+from canary import download_corpus
+from urllib.error import HTTPError
+
+
+class Test(TestCase):
+
+    def test_download_corpus_exception(self):
+        with self.assertRaises(Exception):
+            download_corpus('corpus_that_does_not_exist')
+
+    def test_download(self):
+        try:
+            download_corpus('us2016', overwrite_existing=True)
+        except HTTPError:
+            self.fail("Error downloading corpus")
