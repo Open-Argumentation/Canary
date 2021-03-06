ee4eed79ee259d2a5d2eedb8b88522a87e6826ed
Christopher Wales <chriswales95@outlook.com>
2021-03-06 23:13:16 +0000

    correct excluded packages

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='canary',
     version='0.0.1',
-    packages=find_packages(exclude=['data', 'etc']),
+    packages=find_packages(exclude=['tests']),
     install_requires=[
         "requests",
         "scikit-learn",
