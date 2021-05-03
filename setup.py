316f7a1cb1125cf63360f51e4482c950e2d43d4a
Christopher Wales <chriswales95@outlook.com>
2021-05-03 19:51:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,34 @@
+import pathlib
+
 from setuptools import setup, find_packages
+
+README = (pathlib.Path(__file__).parent / "README.md").read_text()
 
 setup(
     name='canary-am',
     version='0.0.1',
     packages=find_packages(exclude=['tests']),
+    long_description=README,
+    python_requires=">=3.6",
+    long_description_content_type="text/markdown",
+    license="MIT",
     install_requires=[
         "requests",
         "scikit-learn",
         "nltk",
         "vaderSentiment"
     ],
+    url='http://openargumentation.org',
+    project_urls ={
+        'Source': 'https://github.com/chriswales95/Canary',
+    },
     include_package_data=True,
-    zip_safe=False
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence'
+    ]
 )
