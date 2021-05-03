8aaa18efa38b15a3e5de26a2b2d651633e3862b9
Christopher Wales <chriswales95@outlook.com>
2021-05-03 19:10:16 +0100

    Rename package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='canary',
+    name='canary-am',
     version='0.0.1',
     packages=find_packages(exclude=['tests']),
     install_requires=[
