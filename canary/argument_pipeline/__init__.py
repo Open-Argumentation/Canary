e03799d68b303cc36555d26554d29c6361951e99
Christopher Wales <chriswales95@outlook.com>
2021-05-17 22:51:04 +0100

    Tidy up download functionality.

--- canary/argument_pipeline/__init__.py
+++ canary/argument_pipeline/__init__.py
@@ -15,20 +15,32 @@ def download_pretrained_models(location=None, download_to=None, overwrite=False)
     :param overwrite: overwrite existing models
     """
 
+    def unzip_models():
+        with zipfile.ZipFile(download_to, "r") as zf:
+            zf.extractall(Path(MODEL_STORAGE_LOCATION))
+            logger.debug("models extracted.")
+
     if location is None:
         location = MODEL_DOWNLOAD_LOCATION
 
     if download_to is None:
         download_to = Path(MODEL_STORAGE_LOCATION / "models.zip")
 
+    model_zip_present = os.path.isfile(download_to)
+
     # Let's get the pretrained stuff
-    if os.path.isfile(download_to) is False or overwrite is True:
+    if model_zip_present is False or overwrite is True:
         response = requests.get(location, stream=True)
+
         if response.status_code == 200:
             with open(download_to, "wb") as f:
                 f.write(response.raw.read())
                 f.close()
+                logger.debug("Models downloaded! Attempting to unzip.")
 
-            with zipfile.ZipFile(download_to, "r") as zf:
-                zf.extractall(Path(MODEL_STORAGE_LOCATION))
-                logger.debug("Models downloaded!")
+            unzip_models()
+        else:
+            logger.error(f"There was an issue getting the models. Http response code: {response.status_code}")
+    elif model_zip_present is True:
+        logger.debug("model zip already present.")
+        unzip_models()
