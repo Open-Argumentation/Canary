e5d5989ca91a138353d17a995f86a38d67072cb3
Christopher Wales <chriswales95@outlook.com>
2021-05-30 00:01:44 +0100

    Fix tests hopefully. importlib seems to cause issues.

--- canary/__init__.py
+++ canary/__init__.py
@@ -1,7 +1,6 @@
 import logging
 import os as _os
 from configparser import ConfigParser as _ConfigParser
-import importlib.metadata
 
 # Set the logging level from the config file.
 # This logger should be used wherever possible
@@ -12,6 +11,3 @@ logger.setLevel(logging.INFO)
 # Should be used wherever possible
 config = _ConfigParser()
 config.read(_os.path.join(_os.path.dirname(_os.path.abspath(__file__)), 'etc/canary.cfg'))
-
-# Handy to have. Similar to how pandas does it.
-__version__ = importlib.metadata.version('canary')
