52e5c54015ef2f9d9d662a58f74d5c97aea99d83
Christopher Wales <chriswales95@outlook.com>
2021-08-24 21:14:32 +0100

    Correct existing tests and add a new one which checks model _model property is not None

--- tests/test_argument_pipeline.py
+++ tests/test_argument_pipeline.py
@@ -9,14 +9,25 @@ class ArgumentPipeline(TestCase):
     Tests for canary.argument_pipeline
     """
 
+    def __init__(self, methodName: str = ...) -> None:
+        super().__init__(methodName)
+        self.argument_detector = None
+        self.structure_predictor = None
+        self.argument_component = None
+        self.arg_segmenter = None
+
     def setUp(self) -> None:
         """
         Setup for the tests.
         """
         from canary.argument_pipeline import download_pretrained_models
 
-        super().setUp()
         download_pretrained_models("all")
+        self.argument_detector = canary.load("argument_detector")
+        self.structure_predictor = canary.load("structure_predictor")
+        self.argument_component = canary.load("argument_component")
+        self.arg_segmenter = canary.load("arg_segmenter")
+        super().setUp()
 
     def test_download_models(self) -> None:
         """
@@ -28,26 +39,25 @@ class ArgumentPipeline(TestCase):
         self.assertTrue(len(models) > 0)
 
     def test_arg_detection_model_type(self):
-        argument_detector = canary.load("argument_detector")
         from canary.argument_pipeline.classification import ArgumentDetector
 
-        self.assertTrue(type(argument_detector) is ArgumentDetector)
+        self.assertTrue(type(self.argument_detector) is ArgumentDetector)
 
     def test_arg_segmenter_model_type(self):
-        argument_detector = canary.load("arg_segmenter")
         from canary.argument_pipeline.argument_segmenter import ArgumentSegmenter
 
-        self.assertTrue(type(argument_detector) is ArgumentSegmenter)
+        self.assertTrue(type(self.arg_segmenter) is ArgumentSegmenter)
 
     def test_arg_component_model_type(self):
-        argument_detector = canary.load("argument_component")
         from canary.argument_pipeline.component_identification import ArgumentComponent
 
-        self.assertTrue(type(argument_detector) is ArgumentComponent)
+        self.assertTrue(type(self.argument_component) is ArgumentComponent)
 
     def test_arg_structure_model_type(self):
-        argument_detector = canary.load("structure_predictor")
         from canary.argument_pipeline.structure_prediction import StructurePredictor
 
-        self.assertTrue(type(argument_detector) is StructurePredictor)
+        self.assertTrue(type(self.structure_predictor) is StructurePredictor)
 
+    def test_check_models_have_model_data(self):
+        self.assertTrue(model._model is not None for model in
+                        [self.arg_segmenter, self.argument_component, self.structure_predictor, self.argument_detector])
