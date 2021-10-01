51a8df8f94151ed87b56d12b02373a7aa4a841b0
Christopher Wales <chriswales95@outlook.com>
2021-10-01 13:17:34 +0100

    Fix reference to canary version

--- tests/test_canary.py
+++ tests/test_canary.py
@@ -4,5 +4,5 @@ from unittest import TestCase
 class CanaryTests(TestCase):
 
     def test_version_not_none(self):
-        from canary.version import __version__
+        from canary._version import __version__
         self.assertTrue(__version__ is not None and type(__version__) is str)
